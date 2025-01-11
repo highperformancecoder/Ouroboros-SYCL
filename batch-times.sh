f3b2a3cf4c5e06a8df820c69e5c8ddecc05e437d
Russell Standish <hpcoder@hpcoders.com.au>
2025-01-11 14:25:57 +1100

    bash syntax error

--- batch-times.sh
+++ batch-times.sh
@@ -6,7 +6,7 @@ for numAllocations in 256 512 1024 2048 4096 8192; do
         for c in main_c main_p va_main_c va_main_p vl_main_c vl_main_p; do
             echo -n "$c,$numAllocations,$size,"
             ./$c $numAllocations $size &>log
-            if [ $? eq 0 ]; then
+            if [ $? -eq 0 ]; then
                 grep "Timing Allocation" log|cut -c20-27|tr -d 's'|tr '\n' ','
                 grep "Timing       Free" log|cut -c20-27|tr -d 's'
             else
