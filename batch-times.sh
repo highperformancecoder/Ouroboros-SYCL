b7d298ee4e300338dd400a02f6bb8750556d9e64
Russell Standish <hpcoder@hpcoders.com.au>
2025-01-23 16:59:45 +1100

    Put a max job timeout of 10 seconds to prevent batch-times from hanging.

--- batch-times.sh
+++ batch-times.sh
@@ -7,15 +7,27 @@ while [ $numAllocations -le 8192 ]; do
     while [ $size -lt 8192 ] ; do
         for c in main_c main_p va_main_c va_main_p vl_main_c vl_main_p; do
             echo -n "$c,$numAllocations,$size,"
-            ./$c $numAllocations $size &>log
+            ./$c $numAllocations $size &>log &
+            pid=`jobs -p`
+            sleep 10&
+            wait -n
             if [ $? -eq 0 ]; then
-                grep "Timing Allocation" log|cut -c20-27|tr -d '\nms'
-                echo -n ','
-                grep "Timing       Free" log|cut -c20-27|tr -d '\nms'
-                echo ""
+                # possibly still running
+                jobs -p>jobs
+                if grep $pid jobs>/dev/null; then
+                    # still running
+                    echo "NaN,NaN"
+                else
+                    grep "Timing Allocation" log|cut -c20-27|tr -d '\nms'
+                    echo -n ','
+                    grep "Timing       Free" log|cut -c20-27|tr -d '\nms'
+                    echo ""
+                fi
             else
                 echo "NaN,NaN"
             fi
+            disown
+            for pid in `jobs -p`; do kill $pid; done
             sleep 1
         done
         size=$[size+1000]
